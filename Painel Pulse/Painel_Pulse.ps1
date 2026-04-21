a9d6251f2593c1a76d622e6db1b45b715e37a773
cbarboza02 <cbtrem2@gmail.com>
2026-04-21 16:59:15 -0300

    Update Painel_Pulse.ps1

--- Painel Pulse/Painel_Pulse.ps1
+++ Painel Pulse/Painel_Pulse.ps1
@@ -1813,13 +1813,8 @@ function New-OptCard {
             $overlayIco = [System.Windows.Controls.TextBlock]::new()
             $overlayIco.FontFamily = [System.Windows.Media.FontFamily]::new("Segoe Fluent Icons")
             $overlayIco.Text = [char]0xf13b
-            $overlayIco.FontSize = 2
-            $overlayIco.FontWeight = 'Regular'
-            # Cor de destaque (ex: um tom de alerta ou o mesmo padrão)
+            $overlayIco.FontSize = 12
             $overlayIco.Foreground = [System.Windows.Media.SolidColorBrush][System.Windows.Media.ColorConverter]::ConvertFromString('#FF4B4B') 
-            $overlayIco.HorizontalAlignment = 'Center'
-            $overlayIco.VerticalAlignment = 'Center'
-            $overlayIco.Margin = [System.Windows.Thickness]::new(0,0,-2,-2)
             $null = $icoFoco.Children.Add($overlayIco)
         } 
         else {
