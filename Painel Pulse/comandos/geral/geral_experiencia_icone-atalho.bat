479b041a1b3a626233cb74a1888b7c9358bdd09c
cbarboza02 <cbtrem2@gmail.com>
2026-05-25 22:15:05 -0300

    Update geral_experiencia_icone-atalho.bat

--- Painel Pulse/comandos/geral/geral_experiencia_icone-atalho.bat
+++ Painel Pulse/comandos/geral/geral_experiencia_icone-atalho.bat
@@ -14,7 +14,7 @@ del /f /s /q "%LOCALAPPDATA%\Microsoft\Windows\Explorer\iconcache_*.db" >nul 2>&
 powershell -NoProfile -ExecutionPolicy Bypass -Command "$ms=New-Object IO.MemoryStream;$bw=New-Object IO.BinaryWriter($ms);$bw.Write([uint16]0);$bw.Write([uint16]1);$bw.Write([uint16]1);$bw.Write([byte]16);$bw.Write([byte]16);$bw.Write([byte]0);$bw.Write([byte]0);$bw.Write([uint16]1);$bw.Write([uint16]32);$bw.Write([uint32]1128);$bw.Write([uint32]22);$bw.Write([uint32]40);$bw.Write([int32]16);$bw.Write([int32]32);$bw.Write([uint16]1);$bw.Write([uint16]32);$bw.Write([uint32]0);$bw.Write([uint32]0);$bw.Write([int32]0);$bw.Write([int32]0);$bw.Write([uint32]0);$bw.Write([uint32]0);$bw.Write([byte[]]::new(1024));1..16|%%{$bw.Write([byte[]]@(255,255,0,0))};[IO.File]::WriteAllBytes('%SystemRoot%\Blank.ico',$ms.ToArray())"
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v "29" /t REG_SZ /d "%SystemRoot%\Blank.ico" /f
-reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\NamingTemplates" /v "ShortcutNameTemplate" /t REG_SZ /d "%s.lnk" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\NamingTemplates" /v "ShortcutNameTemplate" /t REG_SZ /d "%%s.lnk" /f
 
 start explorer.exe
 exit
